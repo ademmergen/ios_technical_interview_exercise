a9440d0b8d0d18c69f609b08894431ffeb6e369c
amergen28 <adem.mergen@ogr.iuc.edu.tr>
2024-06-25 16:55:11 +0300

    Posts Created

---
+++ Pollexa/Helpers/PostView.swift
@@ -0,0 +1,146 @@
+//
+//  PostView.swift
+//  Pollexa
+//
+//  Created by Adem Mergen on 24.06.2024.
+//
+
+import Foundation
+import UIKit
+
+class PostView: UIView {
+    
+        private let userImageView = UIImageView()
+        private let usernameLabel = UILabel()
+        private let dateLabel = UILabel()
+        private let separatorView = UIView()
+        private let lastVotedLabel = UILabel()
+        private let contentLabel = UILabel()
+        private let optionImageView = UIImageView()
+        private let optionImageView1 = UIImageView()
+        private let totalVotesLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    private func setupView() {
+            self.backgroundColor = .white
+            self.layer.cornerRadius = 25
+            self.layer.masksToBounds = true
+        
+        userImageView.layer.cornerRadius = 16
+        userImageView.clipsToBounds = true
+        
+        usernameLabel.font = .systemFont(ofSize: 17)
+        
+        dateLabel.font = .systemFont(ofSize: 15)
+        dateLabel.textColor = UIColor(named: "DateLabel")
+        
+        let separatorColor = UIColor(red: 120/255, green: 120/255, blue: 128/255, alpha: 0.2)
+        separatorView.backgroundColor = separatorColor
+        
+        lastVotedLabel.font = .systemFont(ofSize: 12, weight: .medium)
+        lastVotedLabel.textColor = UIColor(named: "LastVotedLabel")
+        
+        contentLabel.font = .systemFont(ofSize: 17, weight: .medium)
+        contentLabel.numberOfLines = 0
+        contentLabel.lineBreakMode = .byWordWrapping
+        contentLabel.textColor = UIColor(named: "ContentLabel")
+        
+        totalVotesLabel.font = .systemFont(ofSize: 13)
+        totalVotesLabel.textColor = UIColor(named: "TotalVotesLabel")
+        
+
+            // Alt görselleri ekle
+        
+            addSubview(userImageView)
+            addSubview(usernameLabel)
+            addSubview(dateLabel)
+            addSubview(separatorView)
+            addSubview(lastVotedLabel)
+            addSubview(contentLabel)
+            addSubview(optionImageView)
+            addSubview(optionImageView1)
+            addSubview(totalVotesLabel)
+
+            // Kısıtlamaları kur
+            setupConstraints()
+        }
+    
+    private func setupConstraints() {
+        
+            userImageView.translatesAutoresizingMaskIntoConstraints = false
+            usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+            dateLabel.translatesAutoresizingMaskIntoConstraints = false
+            lastVotedLabel.translatesAutoresizingMaskIntoConstraints = false
+            contentLabel.translatesAutoresizingMaskIntoConstraints = false
+            optionImageView.translatesAutoresizingMaskIntoConstraints = false
+            optionImageView1.translatesAutoresizingMaskIntoConstraints = false
+            totalVotesLabel.translatesAutoresizingMaskIntoConstraints = false
+            separatorView.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                self.widthAnchor.constraint(equalToConstant: 388),
+                self.heightAnchor.constraint(equalToConstant: 338),
+                
+                userImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                userImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 21.2),
+                userImageView.widthAnchor.constraint(equalToConstant: 32),
+                userImageView.heightAnchor.constraint(equalToConstant: 32),
+                
+                usernameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 62),
+                usernameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 25),
+                
+                dateLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+                dateLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 24),
+                
+                separatorView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                separatorView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+                separatorView.topAnchor.constraint(equalTo: self.topAnchor, constant: 63.5),
+                separatorView.heightAnchor.constraint(equalToConstant: 0.5),
+
+                lastVotedLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                lastVotedLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 84),
+
+                contentLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                contentLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+                contentLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 103),
+             
+                optionImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                optionImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 159),
+                optionImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50),
+                optionImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -196),
+                
+                optionImageView1.leadingAnchor.constraint(equalTo: optionImageView.trailingAnchor, constant: 4),
+                optionImageView1.topAnchor.constraint(equalTo: self.topAnchor, constant: 159),
+                optionImageView1.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+                optionImageView1.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50),
+
+                totalVotesLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                totalVotesLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 300),
+            ])
+        }
+    
+    func configure(with post: Post) {
+            userImageView.image = post.user.image
+            usernameLabel.text = post.user.username
+            dateLabel.text = DateFormatter.localizedString(from: post.createdAt, dateStyle: .short, timeStyle: .none)
+            lastVotedLabel.text = "LAST VOTED 1 HOUR AGO"
+            contentLabel.text = post.content
+            
+            if post.options.count > 0 {
+                optionImageView.image = post.options[0].image
+            }
+            if post.options.count > 1 {
+                optionImageView1.image = post.options[1].image
+            }
+            
+            totalVotesLabel.text = "\(post.options.count) Total Votes"
+        }
+}
--- Pollexa/UI Modules/Discover/Controllers/DiscoverViewController.swift
+++ Pollexa/UI Modules/Discover/Controllers/DiscoverViewController.swift
@@ -11,25 +11,37 @@ class DiscoverViewController: UIViewController {
     
     // MARK: - Properties
     private let postProvider = PostProvider.shared
+    private var posts: [Post] = []
+    private let stackView = UIStackView()
+
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupNavigationBar()
+        view.backgroundColor = UIColor(named: "ViewBackground")
         
-        postProvider.fetchAll { result in
-            switch result {
-            case .success(let posts):
-                print(posts)
-                
-            case .failure(let error):
-                debugPrint(error.localizedDescription)
-            }
-        }
+        setupNavigationBar()
+        setupHeaderView()
+        setupStackView()
+        fetchPosts()
+
     }
     
     // MARK: - Setup Methods
+    
+    private func fetchPosts() {
+            postProvider.fetchAll { result in
+                switch result {
+                case .success(let posts):
+                    self.posts = posts
+                    self.setupPostViews()
+                case .failure(let error):
+                    debugPrint(error.localizedDescription)
+                }
+            }
+        }
+    
     private func setupNavigationBar() {
         let navigationBar = UIView()
         navigationBar.translatesAutoresizingMaskIntoConstraints = false
@@ -97,6 +109,92 @@ class DiscoverViewController: UIViewController {
             discoverLabel.heightAnchor.constraint(equalToConstant: 41)
         ])
     }
-}
+    
+    private func setupHeaderView() {
+            let headerView = UIView()
+            headerView.translatesAutoresizingMaskIntoConstraints = false
+            
+            let activePollsLabel = UILabel()
+            activePollsLabel.translatesAutoresizingMaskIntoConstraints = false
+            activePollsLabel.text = "2 Active Polls"
+            activePollsLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+            activePollsLabel.textColor = .white
+            
+            let seeDetailsLabel = UILabel()
+            seeDetailsLabel.translatesAutoresizingMaskIntoConstraints = false
+            seeDetailsLabel.text = "See Details"
+            seeDetailsLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+            seeDetailsLabel.alpha = 0.5
+            seeDetailsLabel.textColor = .white
+            
+            let arrowImageView = UIImageView()
+            arrowImageView.translatesAutoresizingMaskIntoConstraints = false
+            arrowImageView.image = UIImage(systemName: "arrow.right.square.fill")
+            arrowImageView.tintColor = .white
+            
+            headerView.addSubview(activePollsLabel)
+            headerView.addSubview(seeDetailsLabel)
+            headerView.addSubview(arrowImageView)
+            
+            view.addSubview(headerView)
+            
+            NSLayoutConstraint.activate([
+                headerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 170),
+                headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                headerView.heightAnchor.constraint(equalToConstant: 78),
+                
+                activePollsLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
+                activePollsLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15),
+                activePollsLabel.heightAnchor.constraint(equalToConstant: 25),
+                
+                seeDetailsLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
+                seeDetailsLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 42),
+                seeDetailsLabel.heightAnchor.constraint(equalToConstant: 21),
+                
+                arrowImageView.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -21),
+                arrowImageView.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 22),
+                arrowImageView.heightAnchor.constraint(equalToConstant: 33),
+                arrowImageView.widthAnchor.constraint(equalToConstant: 33)
+                
+            ])
+            
+            headerView.backgroundColor = UIColor(named: "AccentColor")
+            headerView.layer.cornerRadius = 16
+        }
+    
+    private func setupStackView() {
+           stackView.axis = .vertical
+           stackView.spacing = 20
+           stackView.translatesAutoresizingMaskIntoConstraints = false
+           view.addSubview(stackView)
+           
+           NSLayoutConstraint.activate([
+               stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 268),
+               stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+               stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+           ])
+       }
+
+       private func setupPostViews() {
+           for post in posts {
+               let postView = PostView()
+               postView.translatesAutoresizingMaskIntoConstraints = false
+               postView.configure(with: post)
+               stackView.addArrangedSubview(postView)
+               
+               NSLayoutConstraint.activate([
+                    postView.widthAnchor.constraint(equalToConstant: 388),
+                   postView.heightAnchor.constraint(equalToConstant: 338)
+               ])
+           }
+       }
+   }
+    
+    
+    
+    
+
+
 
 
