d52853d6a9a773bb6772aefaf78ad533d6166413
amergen28 <adem.mergen@ogr.iuc.edu.tr>
2024-06-25 19:20:03 +0300

    ScrollView Added

--- Pollexa/UI Modules/Discover/Controllers/DiscoverViewController.swift
+++ Pollexa/UI Modules/Discover/Controllers/DiscoverViewController.swift
@@ -12,8 +12,8 @@ class DiscoverViewController: UIViewController {
     // MARK: - Properties
     private let postProvider = PostProvider.shared
     private var posts: [Post] = []
+    private let scrollView = UIScrollView()
     private let stackView = UIStackView()
-
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -23,24 +23,24 @@ class DiscoverViewController: UIViewController {
         
         setupNavigationBar()
         setupHeaderView()
+        setupScrollView()
         setupStackView()
         fetchPosts()
-
     }
     
     // MARK: - Setup Methods
     
     private func fetchPosts() {
-            postProvider.fetchAll { result in
-                switch result {
-                case .success(let posts):
-                    self.posts = posts
-                    self.setupPostViews()
-                case .failure(let error):
-                    debugPrint(error.localizedDescription)
-                }
+        postProvider.fetchAll { result in
+            switch result {
+            case .success(let posts):
+                self.posts = posts
+                self.setupPostViews()
+            case .failure(let error):
+                debugPrint(error.localizedDescription)
             }
         }
+    }
     
     private func setupNavigationBar() {
         let navigationBar = UIView()
@@ -94,7 +94,6 @@ class DiscoverViewController: UIViewController {
             discoverNavigationBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 98),
             discoverNavigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 2),
             discoverNavigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -2),
-            
         ])
         
         let discoverLabel = UILabel()
@@ -111,90 +110,100 @@ class DiscoverViewController: UIViewController {
     }
     
     private func setupHeaderView() {
-            let headerView = UIView()
-            headerView.translatesAutoresizingMaskIntoConstraints = false
+        let headerView = UIView()
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let activePollsLabel = UILabel()
+        activePollsLabel.translatesAutoresizingMaskIntoConstraints = false
+        activePollsLabel.text = "2 Active Polls"
+        activePollsLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        activePollsLabel.textColor = .white
+        
+        let seeDetailsLabel = UILabel()
+        seeDetailsLabel.translatesAutoresizingMaskIntoConstraints = false
+        seeDetailsLabel.text = "See Details"
+        seeDetailsLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        seeDetailsLabel.alpha = 0.5
+        seeDetailsLabel.textColor = .white
+        
+        let arrowImageView = UIImageView()
+        arrowImageView.translatesAutoresizingMaskIntoConstraints = false
+        arrowImageView.image = UIImage(systemName: "arrow.right.square.fill")
+        arrowImageView.tintColor = .white
+        
+        headerView.addSubview(activePollsLabel)
+        headerView.addSubview(seeDetailsLabel)
+        headerView.addSubview(arrowImageView)
+        
+        view.addSubview(headerView)
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 170),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            headerView.heightAnchor.constraint(equalToConstant: 78),
             
-            let activePollsLabel = UILabel()
-            activePollsLabel.translatesAutoresizingMaskIntoConstraints = false
-            activePollsLabel.text = "2 Active Polls"
-            activePollsLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-            activePollsLabel.textColor = .white
+            activePollsLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
+            activePollsLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15),
+            activePollsLabel.heightAnchor.constraint(equalToConstant: 25),
             
-            let seeDetailsLabel = UILabel()
-            seeDetailsLabel.translatesAutoresizingMaskIntoConstraints = false
-            seeDetailsLabel.text = "See Details"
-            seeDetailsLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-            seeDetailsLabel.alpha = 0.5
-            seeDetailsLabel.textColor = .white
+            seeDetailsLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
+            seeDetailsLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 42),
+            seeDetailsLabel.heightAnchor.constraint(equalToConstant: 21),
             
-            let arrowImageView = UIImageView()
-            arrowImageView.translatesAutoresizingMaskIntoConstraints = false
-            arrowImageView.image = UIImage(systemName: "arrow.right.square.fill")
-            arrowImageView.tintColor = .white
-            
-            headerView.addSubview(activePollsLabel)
-            headerView.addSubview(seeDetailsLabel)
-            headerView.addSubview(arrowImageView)
-            
-            view.addSubview(headerView)
-            
-            NSLayoutConstraint.activate([
-                headerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 170),
-                headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                headerView.heightAnchor.constraint(equalToConstant: 78),
-                
-                activePollsLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
-                activePollsLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15),
-                activePollsLabel.heightAnchor.constraint(equalToConstant: 25),
-                
-                seeDetailsLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
-                seeDetailsLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 42),
-                seeDetailsLabel.heightAnchor.constraint(equalToConstant: 21),
-                
-                arrowImageView.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -21),
-                arrowImageView.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 22),
-                arrowImageView.heightAnchor.constraint(equalToConstant: 33),
-                arrowImageView.widthAnchor.constraint(equalToConstant: 33)
-                
-            ])
-            
-            headerView.backgroundColor = UIColor(named: "AccentColor")
-            headerView.layer.cornerRadius = 16
-        }
+            arrowImageView.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -21),
+            arrowImageView.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 22),
+            arrowImageView.heightAnchor.constraint(equalToConstant: 33),
+            arrowImageView.widthAnchor.constraint(equalToConstant: 33)
+        ])
+        
+        headerView.backgroundColor = UIColor(named: "AccentColor")
+        headerView.layer.cornerRadius = 16
+    }
+    
+    private func setupScrollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = false // Scroll çubuğunu gizleme
+        
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 268),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        scrollView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+    }
     
     private func setupStackView() {
-           stackView.axis = .vertical
-           stackView.spacing = 20
-           stackView.translatesAutoresizingMaskIntoConstraints = false
-           view.addSubview(stackView)
-           
-           NSLayoutConstraint.activate([
-               stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 268),
-               stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-               stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-           ])
-       }
-
-       private func setupPostViews() {
-           for post in posts {
-               let postView = PostView()
-               postView.translatesAutoresizingMaskIntoConstraints = false
-               postView.configure(with: post)
-               stackView.addArrangedSubview(postView)
-               
-               NSLayoutConstraint.activate([
-                    postView.widthAnchor.constraint(equalToConstant: 388),
-                   postView.heightAnchor.constraint(equalToConstant: 338)
-               ])
-           }
-       }
-   }
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+    }
     
-    
-    
-    
-
-
+    private func setupPostViews() {
+        for post in posts {
+            let postView = PostView()
+            postView.translatesAutoresizingMaskIntoConstraints = false
+            postView.configure(with: post)
+            stackView.addArrangedSubview(postView)
+            
+            NSLayoutConstraint.activate([
+                postView.widthAnchor.constraint(equalToConstant: 388),
+                postView.heightAnchor.constraint(equalToConstant: 338)
+            ])
+        }
+    }
+}
 
 
